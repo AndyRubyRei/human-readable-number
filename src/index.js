d598a4e9e66bab90b55877f3f417c13394504ec5
Nargiza Dzh <nargizadzh@gmail.com>
2023-10-29 23:33:13 +0600

    feat: problem solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    function convertToWords(num) {
+        if (num < 20) {
+            return ones[num];
+        } else {
+            return tens[Math.floor(num / 10)] + " " + ones[num % 10];
+        }
+    }
+
+    if (number === 0) {
+        return "zero";
+    }
+
+    let words = "";
+
+    if (number >= 1000000) {
+        words += convertToWords(Math.floor(number / 1000000)) + " million ";
+        number %= 1000000;
+    }
+
+    if (number >= 1000) {
+        words += convertToWords(Math.floor(number / 1000)) + " thousand ";
+        number %= 1000;
+    }
+
+    if (number >= 100) {
+        words += convertToWords(Math.floor(number / 100)) + " hundred ";
+        number %= 100;
+    }
+
+    if (number > 0) {
+        words += convertToWords(number);
+    }
+
+    return words.trim();
+};
+
